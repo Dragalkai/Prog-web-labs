e3861c4528f6544310347404a9e455aa7c0744bd
Vincent Jalbert <6291397@cegepmontpetit.ca>
2025-08-27 10:48:09 -0400

    FCT Cours 2 - Composant dynamique
    Création du projet Angular pour le laboratoire 2.

--- labo2/src/app/app.ts
+++ labo2/src/app/app.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('labo2');
+
+  loveList : string[] = ["jeux vidéo", "muscu", "speedrun"];
 }
